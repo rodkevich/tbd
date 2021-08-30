cea8f40be10bd4de580dde1c1bf279a94a152377
404 <ivan@test.it>
2021-08-31 00:00:25 +0300

    upd: или лыжи не едут, или с заданием что-о не то

--- cmd/task4/task4.go
+++ cmd/task4/task4.go
@@ -8,62 +8,88 @@ import (
 	"reflect"
 )
 
+var maxLength = math.Pow(10, 4)
+var maxValue = math.Pow(10, 5) + 1
+
 func main() {
+	// Пример 1: замена большим элементом справа от текущего,
+	// а самый последний в массиве на -1
 	arr1 := [6]int{17, 18, 5, 4, 6, 1}
-	err := someFunction(arr1)
+	_, err := someFunction(arr1)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
-
+	// Пример 2: There are no elements to the right of index 0.
 	arr2 := [1]int{400}
-	err = someFunction(arr2)
+	_, err = someFunction(arr2)
 	if err != nil {
-		return
+		log.Fatal(err)
+	}
+	// 1 &lt;= arr.length &lt;= 10в4
+	var maxLengthFailure = [10001]int{}
+	_, err = someFunction(maxLengthFailure)
+	if err != nil {
+		log.Fatal(err) // NOTE: should fail
+	}
+	// 1 &lt;= arr[i] &lt;= 10в5
+	maxValueFailure := 100001
+	arr3 := [12]int{17, 1, 0, -10, 18, -5, 100000, maxValueFailure, 0, -10, 0, -5}
+	_, err = someFunction(arr3)
+	if err != nil {
+		log.Fatal(err) // NOTE: should fail
 	}
 
-	arr3 := [12]int{17, 1, 0, -10, 18, -5, 17, 12, 0, -10, 0, -5}
-	err = someFunction(arr3)
-	if err != nil {
-		return
-	}
-
-	slice := []int{0, 001, 00001, 0000000001}
-	err = someFunction(slice)
+	wrongTypeFailure := []int{0, 001, 00001, 0000000001}
+	_, err = someFunction(wrongTypeFailure)
 	if err != nil {
 		log.Fatal(err) // NOTE: should fail
 	}
 }
 
-func someFunction(i interface{}) error {
+func someFunction(i interface{}) (interface{}, error) {
 	val := reflect.ValueOf(i)
-	if val.Kind() == reflect.Array {
+	if val.Kind() == reflect.Array && val.Len() <= int(maxLength) {
 		arrLength := val.Len()
-		arr := make([]int64, arrLength)
-		for i := range arr {
+		entities := make([]int64, arrLength, arrLength)
+		printSlice(entities)
+		for i := range entities {
 			e := val.Index(i)
 			switch e.Kind() {
 			case reflect.Int:
-				arr[i] = e.Int()
+				if e.Int() >= int64(int(maxValue)) {
+					return nil, errors.New("usage error")
+				}
+				entities[i] = e.Int()
 			default:
-				return errors.New("not implemented")
+				return nil, errors.New("not implemented")
 			}
 		}
-		printSlice(arr)
+		printSlice(entities)
 		for i := 0; i < arrLength; i++ {
-			tempSlice := arr[i:]
+			tempSlice := entities[i:]
 			var greatest int64 = -math.MaxInt64
 			for _, value := range tempSlice[1:] {
 				if value > greatest {
 					greatest = value
-					arr[i] = greatest
+					entities[i] = greatest
 				}
 			}
 		}
-		arr[arrLength-1] = -1
-		printSlice(arr)
-		return nil
+		entities[arrLength-1] = -1
+		printSlice(entities)
+
+		// "После чего, верните массив entities."
+		// NOTE: я не знаю как мне мутировать и возвращать arrays разной длины
+		// на данный момент нет ...
+		// поэтому пока возвращаю что умею. :)
+		// for ind, el := range entities {
+		// 	val[ind] = el
+		// }
+		// (type 'Value' does not support indexing)
+
+		return entities, nil
 	}
-	return errors.New("invalid argument type: %v")
+	return nil, errors.New("usage error")
 }
 
 func printSlice(s []int64) {
