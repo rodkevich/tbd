cdde532bfbbee2d58f01c08823bfa3b33fab5e87
404 <ivan@test.it>
2021-08-31 01:14:23 +0300

    fix: typos add: usage err var

--- cmd/task4/task4.go
+++ cmd/task4/task4.go
@@ -8,8 +8,12 @@ import (
 	"reflect"
 )
 
-var maxLength = math.Pow(10, 4)
-var maxValue = math.Pow(10, 5) + 1
+var (
+	maxLength = math.Pow(10, 4)
+	maxValue  = math.Pow(10, 5) + 1
+)
+
+var usageError = errors.New("usage error")
 
 func main() {
 	// Пример 1: замена большим элементом справа от текущего,
@@ -19,18 +23,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// Пример 2: There are no elements to the right of index 0.
 	arr2 := [1]int{400}
 	_, err = someFunction(arr2)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// 1 &lt;= arr.length &lt;= 10в4
 	var maxLengthFailure = [10001]int{}
 	_, err = someFunction(maxLengthFailure)
 	if err != nil {
 		log.Fatal(err) // NOTE: should fail
 	}
+
 	// 1 &lt;= arr[i] &lt;= 10в5
 	maxValueFailure := 100001
 	arr3 := [12]int{17, 1, 0, -10, 18, -5, 100000, maxValueFailure, 0, -10, 0, -5}
@@ -46,24 +53,27 @@ func main() {
 	}
 }
 
-func someFunction(i interface{}) (interface{}, error) {
-	val := reflect.ValueOf(i)
+func someFunction(arr interface{}) (interface{}, error) {
+	val := reflect.ValueOf(arr)
+
 	if val.Kind() == reflect.Array && val.Len() <= int(maxLength) {
 		arrLength := val.Len()
 		entities := make([]int64, arrLength, arrLength)
 		printSlice(entities)
+
 		for i := range entities {
 			e := val.Index(i)
 			switch e.Kind() {
 			case reflect.Int:
 				if e.Int() >= int64(int(maxValue)) {
-					return nil, errors.New("usage error")
+					return nil, usageError
 				}
 				entities[i] = e.Int()
 			default:
 				return nil, errors.New("not implemented")
 			}
 		}
+
 		printSlice(entities)
 		for i := 0; i < arrLength; i++ {
 			tempSlice := entities[i:]
@@ -78,7 +88,7 @@ func someFunction(i interface{}) (interface{}, error) {
 		entities[arrLength-1] = -1
 		printSlice(entities)
 
-		// "После чего, верните массив entities."
+		// "После чего, верните массив arr."
 		// NOTE: я не знаю как мне мутировать и возвращать arrays разной длины
 		// на данный момент нет ...
 		// поэтому пока возвращаю что умею. :)
@@ -89,7 +99,7 @@ func someFunction(i interface{}) (interface{}, error) {
 
 		return entities, nil
 	}
-	return nil, errors.New("usage error")
+	return nil, usageError
 }
 
 func printSlice(s []int64) {
