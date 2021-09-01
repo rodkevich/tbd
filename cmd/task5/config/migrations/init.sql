8b03ad116146a821290d57f1fa7d3173d4b1758f
404 <ivan@test.it>
2021-09-01 14:16:58 +0300

    upd: sql

--- cmd/task5/config/migrations/init.sql
+++ cmd/task5/config/migrations/init.sql
@@ -1,22 +1,30 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS ticket
+CREATE TABLE IF NOT EXISTS tickets
 (
-    id           uuid             DEFAULT uuid_generate_v4(),
-    order_number VARCHAR NOT NULL,
-    ticket_name  VARCHAR NOT NULL,
-    description  VARCHAR,
-    is_deleted   BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at   TIMESTAMP,
-    PRIMARY KEY (id)
+    ticket_id       uuid                     DEFAULT uuid_generate_v4(),
+    order_number    NUMERIC(2)      NOT NULL,
+    ticket_name     VARCHAR(200)    NOT NULL,
+    photo_main_link VARCHAR(1000),
+    currency        NUMERIC(5),
+    current_price   NUMERIC(100, 2) NOT NULL DEFAULT 0.00,
+    discount        NUMERIC(3),
+    min_price       NUMERIC(100, 2),
+    max_price       NUMERIC(100, 2),
+    description     VARCHAR(1000),
+    phone_number    VARCHAR(50),
+    is_active       BOOLEAN         NOT NULL DEFAULT FALSE,
+    created_at      VARCHAR(35),
+    PRIMARY KEY (ticket_id)
 );
 
 CREATE TABLE IF NOT EXISTS photo_links
 (
-    id          SERIAL  NOT NULL,
-    hello_world VARCHAR,
-    is_main     BOOLEAN NOT NULL DEFAULT FALSE,
-    is_deleted  BOOLEAN NOT NULL DEFAULT FALSE
-
+    link_id      SERIAL       NOT NULL,
+    ticket_id    uuid,
+    link_address VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_ticket
+        FOREIGN KEY (ticket_id)
+            REFERENCES tickets (ticket_id)
+            ON DELETE CASCADE
 );
-
