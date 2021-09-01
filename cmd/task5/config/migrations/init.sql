d439b04fa67b2fa8d2ed384654a799c26d590903
404 <ivan@test.it>
2021-09-02 00:46:28 +0300

    add: modify types

--- cmd/task5/config/migrations/init.sql
+++ cmd/task5/config/migrations/init.sql
@@ -2,10 +2,10 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS tickets
 (
-    ticket_id       uuid                     DEFAULT uuid_generate_v4(),
+    id              uuid                     DEFAULT uuid_generate_v4(),
     order_number    NUMERIC(2)      NOT NULL,
     ticket_name     VARCHAR(200)    NOT NULL,
-    photo_main_link VARCHAR(1000),
+    photo_main_link VARCHAR(400),
     currency        NUMERIC(5),
     current_price   NUMERIC(100, 2) NOT NULL DEFAULT 0.00,
     discount        NUMERIC(3),
@@ -14,17 +14,20 @@ CREATE TABLE IF NOT EXISTS tickets
     description     VARCHAR(1000),
     phone_number    VARCHAR(50),
     is_active       BOOLEAN         NOT NULL DEFAULT FALSE,
-    created_at      VARCHAR(35),
-    PRIMARY KEY (ticket_id)
+    created_at      TIMESTAMP,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS photo_links
 (
-    link_id      SERIAL       NOT NULL,
+    link_id      BIGSERIAL    NOT NULL,
     ticket_id    uuid,
     link_address VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_ticket
-        FOREIGN KEY (ticket_id)
-            REFERENCES tickets (ticket_id)
-            ON DELETE CASCADE
+--     CONSTRAINT fk_ticket
+    FOREIGN KEY (ticket_id)
+        REFERENCES tickets (id)
+        ON DELETE CASCADE
 );
+
+
+
