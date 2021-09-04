c82844bcce87f55f0acb1f3e010c0dd803595bc6
404 <ivan@test.it>
2021-09-04 12:18:23 +0300

    upd: pg prtfy

--- pkg/datasource/postgress/postgress.go
+++ pkg/datasource/postgress/postgress.go
@@ -47,13 +47,14 @@ func (d datasource) Create(t tickets.Ticket) (ticketID string) {
 	ctx, cancel := context.WithTimeout(ctxDefault, operationsTimeOut)
 	defer cancel()
 
-	stmt = `INSERT INTO tickets
-			(order_number, ticket_name, photo_main_link, currency, current_price,
-			discount, min_price, max_price, description, phone_number,
-			is_active, created_at)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-			RETURNING id;
-			`
+	stmt = `
+		INSERT INTO tickets
+		(order_number, ticket_name, photo_main_link, currency, current_price,
+		discount, min_price, max_price, description, phone_number,
+		is_active, created_at)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+		RETURNING id;
+		`
 	err := d.db.QueryRow(
 		ctx, stmt,
 		t.OrderNumber, t.Name, t.PhotoMainLink, t.Price.Currency,
@@ -69,7 +70,8 @@ func (d datasource) Create(t tickets.Ticket) (ticketID string) {
 		stmt2 := `
 			INSERT INTO photo_links (ticket_id, link_address)
 			VALUES ($1, $2)
-			RETURNING link_id;`
+			RETURNING link_id;
+		`
 		err := d.db.QueryRow(ctx, stmt2, ticketID, photoLink).Scan(&c)
 		if err != nil {
 			log.Println(err)
@@ -81,11 +83,19 @@ func (d datasource) Create(t tickets.Ticket) (ticketID string) {
 // List ...
 func (d datasource) List(PriceSort, DateSort string) (items []tickets.Ticket) {
 	stmt := `
-	SELECT *
-	FROM tickets
-	ORDER BY
-	created_at ` + DateSort + `,
-	current_price ` + PriceSort + `;
+		WITH some_count AS (
+		SELECT id, order_number, ticket_name, photo_main_link, currency, current_price,
+		discount, min_price, max_price, description, phone_number,
+		is_active, created_at,
+		ARRAY_AGG(link_address) photo_links
+		FROM tickets
+		LEFT JOIN photo_links ON tickets.id = photo_links.ticket_id
+		GROUP BY id)
+		SELECT *
+		FROM some_count
+		ORDER BY
+		created_at ` + DateSort + `,
+		current_price ` + PriceSort + `;
 	`
 	rows, err := d.db.Query(ctxDefault, stmt)
 	if err != nil {
@@ -97,31 +107,11 @@ func (d datasource) List(PriceSort, DateSort string) (items []tickets.Ticket) {
 		var t tickets.Ticket
 		if err := rows.Scan(
 			&t.ID, &t.OrderNumber, &t.Name, &t.PhotoMainLink,
-			&t.Price.Currency, &t.Price.Current, &t.Price.Discount,
-			&t.Price.Min, &t.Price.Max,
-			&t.Description, &t.PhoneNumber,
-			&t.Active, &t.DateCreated,
+			&t.Price.Currency, &t.Price.Current,
+			&t.Price.Discount, &t.Price.Min, &t.Price.Max,
+			&t.Description, &t.PhoneNumber, &t.Active, &t.DateCreated, &t.PhotoLinks,
 		); err != nil {
 			return nil
-		}
-
-		getTicketLinks := `
-		SELECT  link_address
-		FROM photo_links
-		WHERE ticket_id = $1;
-		`
-		rows, err := d.db.Query(ctxDefault, getTicketLinks, t.ID)
-		if err != nil {
-			log.Println(err)
-		}
-		var link string
-		for rows.Next() {
-			if err := rows.Scan(
-				&link,
-			); err != nil {
-				return
-			}
-			t.PhotoLinks = append(t.PhotoLinks, link)
 		}
 		items = append(items, t)
 	}
@@ -144,7 +134,7 @@ func (d datasource) TicketWithID(id uuid.UUID, fields bool) *tickets.Ticket {
 		is_active, created_at,
 		ARRAY_AGG(link_address) photo_links
 		FROM tickets
-		 LEFT JOIN photo_links ON tickets.id = photo_links.ticket_id
+		LEFT JOIN photo_links ON tickets.id = photo_links.ticket_id
 		GROUP BY id)
 		SELECT *
 		FROM some_count
